9a3926ab174fcb7c8b1b2155c12d901fcaa01e23
Pavan Kuchana <pavankuchana007@gmail.com>
2022-02-22 17:43:37 +0530

    Todo Manager VueJS VueX

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+   <AddTodo/>
+   <FilterTodos/>
+   <Todos/>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
+import FilterTodos from './components/FilterTodos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodos
+  }
+}
+</script>
+
+<style>
+
+</style>
